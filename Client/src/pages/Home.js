d1a3d427f42c25166193eac93728cf8b8ae395b3
thanhthiengcd18708 <66301633+thanhthiengcd18708@users.noreply.github.com>
2022-05-11 01:42:50 +0700

    Create homepage and product detail

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,7 +1,43 @@
 import React from "react";
+import Jumbotron from "../components/cards/Jumbotron";
+import NewArrivals from "../components/home/NewArrivals";
+import BestSellers from "../components/home/BestSellers";
+import CategoryList from "../components/category/CategoryList";
+import SubList from "../components/sub/SubList";
 
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  return (
+    <>
+      <div className="jumbotron text-danger h1 font-weight-bold text-center">
+        <Jumbotron
+          text={["Final Project", "Oder During Covid-19", "05-12-2022"]}
+        />
+      </div>
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        New Arrivals
+      </h4>
+      <NewArrivals />
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        Best Sellers
+      </h4>
+      <BestSellers />
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        Categories
+      </h4>
+      <CategoryList />
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        Sub Categories
+      </h4>
+      <SubList />
+
+      <br />
+      <br />
+    </>
+  );
+};
 
 export default Home;
