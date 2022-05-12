1a9daecd71728b0ffe40c82e977864208c8bf874
thanhthiengcd18708 <66301633+thanhthiengcd18708@users.noreply.github.com>
2022-05-12 15:22:37 +0700

    da

---
+++ Server/models/coupon.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const couponSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      unique: true,
+      uppercase: true,
+    },
+    expiry: {
+      type: Date,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      requred: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Coupon", couponSchema);
